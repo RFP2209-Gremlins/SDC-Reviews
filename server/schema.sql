fd33d58260b3badd4a7c381a254c9fd8ba47c0be
Joseph <jredboyz@gmail.com>
2022-11-02 21:19:50 -0700

    fixed err with reviews table

--- server/schema.sql
+++ server/schema.sql
@@ -1,17 +1,20 @@
-CREATE TABLE IF NOT EXISTS reviews (
-  id serial NOT NULL PRIMARY KEY,
-  product_id INT NOT NULL,
-  rating INT NOT NULL,
-  date timestamp NOT NULL,
-  summary VARCHAR(255) NOT NULL,
-  body VARCHAR(255) NOT NULL,
-  recommend boolean NOT NULL,
-  reported boolean NOT NULL,
-  reviewer_name VARCHAR(50) NOT NULL,
-  reviewer_email VARCHAR(50) NOT NULL,
-  response VARCHAR(255),
-  helfulness INT
-);
+
+-- DROP TABLE reviews CASCADE;
+
+-- CREATE TABLE IF NOT EXISTS reviews (
+--   id serial NOT NULL PRIMARY KEY,
+--   product_id INT NOT NULL,
+--   rating INT NOT NULL,
+--   date VARCHAR(50) NOT NULL,
+--   summary VARCHAR(255) NOT NULL,
+--   body VARCHAR(1000) NOT NULL,
+--   recommend boolean NOT NULL,
+--   reported boolean NOT NULL,
+--   reviewer_name VARCHAR(50) NOT NULL,
+--   reviewer_email VARCHAR(50) NOT NULL,
+--   response VARCHAR(255),
+--   helfulness INT
+-- );
 
 CREATE TABLE IF NOT EXISTS reviews_photos (
   id serial NOT NULL PRIMARY KEY,
